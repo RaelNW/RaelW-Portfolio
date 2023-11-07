f1cfa6279134f1d4c6b54e11b839612e91390801
Rael Wanjala <raelnwanjala@gmail.com>
2023-11-06 22:35:00 -0800

    imported individual pages and created routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,49 @@
 import ReactDOM from 'react-dom/client'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.jsx'
 import Project from './components/Project.jsx'
 //import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
+import Error from './pages/Error.jsx'
+import AboutMe from './pages/AboutMe.jsx'
+import Portfolio from './pages/Portfolio.jsx'
+import Contact from './pages/Contact.jsx'
+
+const routes =createBrowserRouter ([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <AboutMe />
+      },
+      {
+        path: '/portfolio',
+        element: <Portfolio />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
+      },
+      {
+        path: '/projects',
+        element: <Project />
+      },
+      {
+        path: '*',
+        element: <Error />
+      },
+    ]
+  },
+]);
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+  <RouterProvider router={routes} />
+);
