b4aaa5d5be2858797e0fcdc2681ec534ee942aae
Rael Wanjala <raelnwanjala@gmail.com>
2023-11-06 22:16:41 -0800

    Added code to index.html, app.jsx, main.jsx, portfolio.jsx, changed the config.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Project from './components/Project.jsx'
 //import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
-
-
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
