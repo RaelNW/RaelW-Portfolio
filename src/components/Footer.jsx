af5d5d59d85e6805c9a28769bde0f7928e086d0b
Rael Wanjala <raelnwanjala@gmail.com>
2023-11-08 18:15:10 -0800

    Added imported images to footer and added base to config

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import github from '../../public/images/download.png';
+import insta from '../../public/images/insta-icon.jpeg';
+import linkedin from '../../public/images/linkedinicon.png';
 
 export function Footer() {
    return (
@@ -13,12 +15,12 @@ export function Footer() {
                   </li>
                   <li className="footer">
                     <a href="https://www.instagram.com/dance.dada/" target="_blank" rel="noopener noreferrer">
-                      <img src='public/images/insta-icon.jpeg' alt="Instagram" />
+                      <img src={insta} alt="Instagram" />
                     </a>
                   </li>
                   <li className="footer">
                     <a href="https://www.linkedin.com/in/raelwanjala/" target="_blank" rel="noopener noreferrer">
-                      <img src="public/images/linkedinicon.png" alt="LinkedIn" />
+                      <img src={linkedin} alt="LinkedIn" />
                     </a>
                   </li>
                </ul>
